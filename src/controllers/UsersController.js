2143f11ac1c8cdcafafc4eeb15943889e1f652a8
Diatoufall <diatoufall278@gmail.com>
2025-08-07 22:50:40 -0400

    implémentation register/login

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,36 @@
 ﻿const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../models/db');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Users endpoint');
+// Register
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  db.run(
+    'INSERT INTO users (username, password) VALUES (?, ?)',
+    [username, hashedPassword],
+    function (err) {
+      if (err) return res.status(400).json({ error: err.message });
+      res.status(201).json({ id: this.lastID });
+    }
+  );
+});
+
+// Login
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  
+  db.get('SELECT * FROM users WHERE username = ?', [username], async (err, user) => {
+    if (err || !user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    
+    const token = jwt.sign({ id: user.id }, 'SECRET_KEY', { expiresIn: '1h' });
+    res.json({ token });
+  });
 });
 
 module.exports = router;
