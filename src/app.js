14e9c48f7afb4c1bc1f628369ea5374fc6489281
Diatoufall <diatoufall278@gmail.com>
2025-08-07 23:10:38 -0400

    Modification app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,41 @@
 ﻿const express = require('express');
 const app = express();
+
+// Import des contrôleurs
 const usersRouter = require('./controllers/UsersController');
 const eventsRouter = require('./controllers/EventsController');
+const commentsRouter = require('./controllers/CommentsController');
+const rsvpRouter = require('./controllers/RSVPController');
 
-// Middlewares
+// Middleware
 app.use(express.json());
 
-// Routes
+// Route racine
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'API Event Management fonctionnelle!',
+    endpoints: {
+      users: '/api/users',
+      events: '/api/events',
+      comments: '/api/comments',
+      rsvp: '/api/rsvp'
+    }
+  });
+});
+
+// Routes API
 app.use('/api/users', usersRouter);
 app.use('/api/events', eventsRouter);
+app.use('/api/comments', commentsRouter);
+app.use('/api/rsvp', rsvpRouter);
 
-// Error handler
+// Gestion des erreurs
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(500).json({ 
+    error: 'Internal Server Error',
+    message: err.message 
+  });
 });
 
 module.exports = app;
